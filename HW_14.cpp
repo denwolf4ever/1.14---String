bac378fa384b7ebb48624ce80eaa61bdc6493484
denwolf4ever <denwolf4ever@gmail.com>
2024-04-11 13:29:05 +0300

    first commit

---
+++ HW_14.cpp
@@ -0,0 +1,17 @@
+﻿// HW_14.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string stroka;
+    getline(cin,stroka);
+    cout << stroka << endl;
+    cout << stroka.length() << endl;
+    cout << stroka[0] << endl;
+    cout << stroka[stroka.length() - 1] << endl;
+}
+
